7628b2b648f400dc43d9f11f570ae2e47f804a96
Hasan <hasanizmirr@gmail.com>
2023-03-10 19:19:43 +0300

    feat: Toast added for error handling

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
+import Vue3Toasity from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(router).use(Vue3Toasity).mount("#app");
