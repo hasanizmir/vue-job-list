a519bd0deded5c5300240c967b086dee8be777de
Hasan <hasanizmirr@gmail.com>
2023-03-10 19:21:22 +0300

    feat: Job stores created

---
+++ src/store/jobDetail.js
@@ -0,0 +1,24 @@
+import api from "@/services/api";
+
+export default {
+  state: {
+    job: [],
+  },
+  getters: {
+    getJob: (state) => state.job,
+  },
+  mutations: {
+    setJob(state, job) {
+      state.job = job;
+    },
+  },
+  actions: {
+    async fetchJob({ state, commit }, id) {
+      if (state.jobs?.length > 0) return;
+
+      const job = await api.get(`/jobs/${id}`);
+      commit("setJob", job.data);
+    },
+  },
+  namespaced: true,
+};
---
+++ src/store/jobs.js
@@ -0,0 +1,24 @@
+import api from "@/services/api";
+
+export default {
+  state: {
+    jobs: [],
+  },
+  getters: {
+    getJobs: (state) => state.jobs,
+  },
+  mutations: {
+    setJobs(state, jobs) {
+      state.jobs = jobs;
+    },
+  },
+  actions: {
+    async fetchJobs({ state, commit }) {
+      if (state.jobs?.length > 0) return;
+
+      const jobs = await api.get("/jobs");
+      commit("setJobs", jobs.data);
+    },
+  },
+  namespaced: true,
+};
