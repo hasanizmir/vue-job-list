dd4d71736023f22dabdf4bafeb1b836079453836
Hasan <hasanizmirr@gmail.com>
2023-03-11 18:53:49 +0300

    feat: Components added to Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,31 @@
 <template>
   <div class="home">
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <h1 class="home__header">Kariyer Fırsatlarını Keşfet</h1>
+    <SearchArea />
+    <JobItems :title="'Son ilanlar'" />
   </div>
 </template>
 
 <script>
+import JobItems from "@/components/JobItems.vue";
+import SearchArea from "@/components/SearchArea.vue";
+
 export default {
   name: "HomeView",
+  components: {
+    JobItems,
+    SearchArea,
+  },
 };
 </script>
+<style lang="scss" scoped>
+.home {
+  &__header {
+    max-width: var(--container);
+    font-size: var(--font-size-3xl);
+    font-weight: var(--font-weight-semibold);
+    margin: var(--space-md) auto;
+    padding: var(--space-md);
+  }
+}
+</style>
